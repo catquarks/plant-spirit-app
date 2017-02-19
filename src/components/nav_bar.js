da1b1928bbebfabb89b197cb516691dc363fbe7d
Tami <catquarks@gmail.com>
2017-02-18 19:46:52 -0500

    add navbar links

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -11,8 +11,8 @@ export default function() {
 	      	</a>
 
 		      <ul className="right">
-		        <li><a href="sass.html" className="waves-effect waves-light">How to Use This App</a></li>
-		        <li><a href="badges.html" className="waves-effect waves-light">About Bethany</a></li>
+		        <li><a href="#" className="waves-effect waves-light">How to Use This App</a></li>
+		        <li><a href="#" className="waves-effect waves-light">About Bethany</a></li>
 		        <li><a href="http://www.oluminousbeing.com/" target="_blank" className="waves-effect waves-light">O Luminous Being</a></li>
 		      </ul>
 		    </div>
