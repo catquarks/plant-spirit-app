8b9cba1a9420404d1e896328a9f3260d7848188d
Tami <catquarks@gmail.com>
2017-02-18 21:35:38 -0500

    sass

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -3,20 +3,20 @@ import React from 'react'
 export default function() {
 
 	return(
-
+		<div className="navbar-fixed">
 		  <nav>
 		    <div className="nav-wrapper">
 		      <a href="#!" className="brand-logo left">
 		      	Plant Spirit App
 	      	</a>
 		      <ul className="right">
-		        <li><a href="#">How to Use This App</a></li>
-		        <li><a href="#">About Béthany</a></li>
-		        <li><a href="http://www.oluminousbeing.com/" target="_blank">O Luminous Being</a></li>
+		        <li><a href="#" className="waves-effect">How to Use This App</a></li>
+		        <li><a href="#" className="waves-effect">About Béthany</a></li>
+		        <li><a href="http://www.oluminousbeing.com/" target="_blank" className="waves-effect">O Luminous Being</a></li>
 		      </ul>
 		    </div>
 		  </nav>
-
+	  </div>
 
 	)
 
