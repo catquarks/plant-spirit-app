494aa09702a0c10194863b6339f3f812715704b2
Tami <catquarks@gmail.com>
2017-02-18 19:44:56 -0500

    finish star theme

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -9,11 +9,10 @@ export default function() {
 		      <a href="#!" className="brand-logo left">
 		      	Plant Spirit App
 	      	</a>
-
 		      <ul className="right">
-		        <li><a href="sass.html" className="waves-effect waves-light">How to Use This App</a></li>
-		        <li><a href="badges.html" className="waves-effect waves-light">About Bethany</a></li>
-		        <li><a href="http://www.oluminousbeing.com/" target="_blank" className="waves-effect waves-light">O Luminous Being</a></li>
+		        <li><a href="#">How to Use This App</a></li>
+		        <li><a href="#">About Béthany</a></li>
+		        <li><a href="http://www.oluminousbeing.com/" target="_blank">O Luminous Being</a></li>
 		      </ul>
 		    </div>
 		  </nav>
