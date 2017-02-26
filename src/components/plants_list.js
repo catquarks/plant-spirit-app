53f99ea3fa666f3bcf029e53b6efe00c4e59b063
Tami <catquarks@gmail.com>
2017-02-25 22:15:07 -0500

    made functional component from plants_list

--- src/components/plants_list.js
+++ src/components/plants_list.js
@@ -2,28 +2,16 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PlantShow from './plant_show'
 
-class PlantsList extends React.Component {
-
-	render(){
-		return(
-			<div id="plants-list">
-				<div className="plant-show-container">
-				{this.props.plants.map(plant => {
-					return(
-						<PlantShow key={plant.id} name={plant.name} summary={plant.summary} thumb={plant.thumb_pic} medium={plant.medium_pic} large={plant.large_pic} />
-					)
-				})}
-				</div>
+export default function(props){
+	return(
+		<div id="plants-list">
+			<div className="plant-show-container">
+			{props.plants.map(plant => {
+				return(
+					<PlantShow key={plant.id} name={plant.name} summary={plant.summary} thumb={plant.thumb_pic} medium={plant.medium_pic} large={plant.large_pic} />
+				)
+			})}
 			</div>
-		)
-	}
+		</div>
+	)
 }
-
-function mapStateToProps(state){
-	return({
-		plants: state.plants,
-		currentFeeling: state.currentFeeling
-	})
-}
-
-export default connect(mapStateToProps)(PlantsList)
