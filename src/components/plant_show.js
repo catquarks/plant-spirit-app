15edc440afa2735406e24e45fffcf3c90ca38222
Tami <catquarks@gmail.com>
2017-03-04 20:32:03 -0500

    flexbox

--- src/components/plant_show.js
+++ src/components/plant_show.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function(props){
 	return(
-		<div className="plant-show card">
+		<div className="card plant-show">
 			<div className="image-area card-image">
 				<img src={props.large} target="_blank" alt={props.name} />
 				<span className="card-title title">{props.name}</span>
