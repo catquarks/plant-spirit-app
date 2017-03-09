a491a123e5c924e4d260abfd2257213e9ae5395a
Tami <catquarks@gmail.com>
2017-03-09 17:50:54 -0500

    blank feeling summaries refer user to Google or AlleyDog

--- src/components/feeling_show.js
+++ src/components/feeling_show.js
@@ -2,10 +2,35 @@ import React from 'react'
 
 export default function(props){
 
+	function google(term){
+		return(
+			<a href={`https://www.google.com/#q=${term}`}
+				target="_blank"
+				>Google</a>
+		)
+	}
+
+	function alleydog(term){
+		return(
+			<a href={`https://www.alleydog.com/glossary/definition.php?term=${term}`}
+				target="_blank"
+				>AlleyDog</a>
+		)
+	}
+
+	function noSummary(props){
+		return(
+			<span>
+				No summary is currently available. In the meantime, you can click to search&nbsp;
+				{google(props.name)} or {alleydog(props.name)}.
+			</span>
+		)
+	}
+
 	return(
 		<div id="feeling-show">
 			<h3>{props.name}</h3>
-			<pre>{props.summary}</pre>
+			<pre>{props.summary ? props.summary : noSummary(props)}</pre>
 		</div>
 	)
 }
